88c8e1f7562dc6892c3dd05b44e14a6c7b0582c0
Billy Clayson <billyclayson0@gmail.com>
2024-01-24 14:01:19 -0500

    more strings practice

--- strings.py
+++ strings.py
@@ -1,4 +1,4 @@
-#adding a comment in the remote repo
+# adding a comment in the remote repo
 '''
 print("Hello World")
 # for variable names, do not use reserved keywords
@@ -61,7 +61,7 @@ print(first_name + " " + last_name + ": " + str(age))
 # string multiplication
 print("hello"*3)
 # can only add 2 strings and can only multiply a string with an int
-'''
+
 
 # accessing string characters
 emp_name = "Jane Doe"
@@ -69,5 +69,29 @@ print(emp_name[3])
 # print(emp_name[8]) # throws index out of bounds error because the last char is at idx 7
 
 print(emp_name.index('n'))
+'''
 
+# string slicing #
+emp_name = "Jane Doe"
+print(emp_name[2:6])  # chars from 2 to 6, not including 6
+print(emp_name[:4])  # starts from start until 4, not including 4
+print(emp_name[2:])  # from 2 til the end
+print(emp_name[-4:-1])  # -1 is the last char so -4 is 3 before that
+print(emp_name[1:6:2])  # from 1 to 6 by 2
+
+print(emp_name.count('e'))
+print(emp_name.find('Doe'))
+print(emp_name.replace('Jane', 'John'))
+
+emp_name = emp_name.replace('Jane', 'John')
+print('oh' in emp_name)
+
+# String Formatting
+student_name = "Alice"
+score = 87
+print(student_name + ": " + str(score))  # need str to print the number
+print("Name: {} Score: {}".format (student_name, score))
+# f-strings
+print(f'Name: {student_name} Score: {score}')
+print(f'10 times 3 is {10*3}')
 
