57e20d51f0870c4cef64a8152f24bb3305ab8a15
Billy Clayson <billyclayson0@gmail.com>
2024-01-24 14:17:53 -0500

    question 2

--- string_excersizes.py
+++ string_excersizes.py
@@ -7,7 +7,7 @@ print(test_string[0])
 print(test_string[-1])
 mid_index = int(len(test_string)/2)
 print(test_string[mid_index])
-'''
+
 
 user_string = input("please enter a string: ")
 print(user_string)
@@ -22,4 +22,25 @@ mid_char = user_string[mid_index]  # to access the char at mid_index
 
 result_str = first_char + mid_char + last_char
 print(result_str)
+'''
+
+# Question 2: : Take an input string from the user and create a new string made of the middle three
+# characters of the input string
+
+user_string = input("Please enter a string")
+print(user_string)
+
+str_length = len(user_string)
+mid_index = int(str_length/2)
+mid_char = user_string[mid_index]
+print(mid_char)
+
+left_index = mid_index - 1
+right_index = mid_index + 1
+
+left_char = user_string[left_index]
+right_char= user_string[right_index]
+
+print(left_char + mid_char + right_char)
+
 
