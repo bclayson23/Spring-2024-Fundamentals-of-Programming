cc8880d502e2f91d304bee5c46245f63f12700a0
Billy Clayson <billyclayson0@gmail.com>
2024-01-29 14:37:24 -0500

    doing the problems

--- string_excersizes.py
+++ string_excersizes.py
@@ -27,6 +27,7 @@ print(result_str)
 # Question 2: : Take an input string from the user and create a new string made of the middle three
 # characters of the input string
 
+'''
 user_string = input("Please enter a string")
 print(user_string)
 
@@ -42,5 +43,46 @@ left_char = user_string[left_index]
 right_char= user_string[right_index]
 
 print(left_char + mid_char + right_char)
+'''
+
+# Question 3: : Take 2 strings as inputs from the user. Append the second string in the middle of the first
+# string
+
+'''
+input_str1 = input("please enter the first string: ")
+input_str2 = input("please enter the second string: ")
+
+print(input_str1)
+mid_index = int(len(input_str1)/2)
+
+first_half = input_str1[:mid_index:1]
+
+first_half = first_half + input_str2
+
+final_str = first_half + input_str1[mid_index:]
+
+print(final_str)
+'''
+
+# Question 4: Take a string from the user and reverse it
+
+'''
+input_str = input('Please enter a string: ')
+print(input_str)
+reverse_str = input_str[::-1]
+print(reverse_str)
+'''
+
+# Question 5:  Extract the amount from the below string
+
+'''
+ given_str = "The total value of the 10 products purchased along with the tax is $150"
+ print(given_str[-3:1])
+'''
+
+# Question 6:  Try to change the 4th character of a given string. Were you able to do it?
+# to do this you would use something like str[3] = 'r'
+
+
 
 
