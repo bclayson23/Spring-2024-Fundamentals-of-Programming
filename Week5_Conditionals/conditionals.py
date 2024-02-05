69ac9d8cfc0c089c80a27d9e06e99128ffca1de1
Billy Clayson <billyclayson0@gmail.com>
2024-02-05 14:01:35 -0500

    conditional work

---
+++ Week5_Conditionals/conditionals.py
@@ -0,0 +1,100 @@
+# if-else
+'''
+height = int(input("Please enter your height in cm: "))
+
+if height > 120:
+    print("Can Ride")
+
+else:
+    print("Can't Ride")
+
+
+#nested if-else
+
+height = int(input("Please enter your height in cm: "))
+
+if height > 120:
+    print("Can Ride")
+    age = int(input("Enter your age: "))
+    if age > 18:
+        print("Ticket is $12")
+    else:
+        print("Ticket is $7")
+else:
+    print("Can't Ride")
+
+
+# nested if-else with elif
+height = int(input("Please enter your height in cm: "))
+bill = 0
+
+if height > 120:
+    print("Can Ride")
+    age = int(input("Enter your age: "))
+    if age < 12:
+        ticket_price = 5
+        print("Ticket is $5")
+    elif 12 <= age < 18:
+        ticket_price = 7
+        print("Ticket is $7")
+    elif 18 <= age < 45:
+        ticket_price = 12
+        print("Ticket is $12")
+    else:
+        ticket_price = 0
+        print("Ticket is free")
+    want_photos = input("Do you want a photo taken? Enter Y/y or N/n: ")
+
+    if want_photos == "Y" or want_photos == "y":
+        bill = ticket_price + 3
+
+    if want_photos == "N" or want_photos == "n":
+        bill = ticket_price
+
+    print(f"Your final bill is ${bill}")
+
+
+else:
+    print("Can't Ride")
+'''
+
+# grades question
+grade = int(input("Enter your number grade: "))
+
+if grade <= 60:
+    print("F")
+
+elif grade <= 70:
+    if grade < 63:
+        print("D-")
+    elif grade < 67:
+        print("D")
+    else:
+        print("D+")
+
+elif grade <= 80:
+    if grade < 73:
+        print("C-")
+    elif grade < 77:
+        print("C")
+    else:
+        print("C+")
+
+elif grade <= 90:
+    if grade < 83:
+        print("B-")
+    elif grade < 87:
+        print("B")
+    else:
+        print("B+")
+
+elif grade <= 100:
+    if grade < 93:
+        print("A-")
+    elif grade < 97:
+        print("A")
+    else:
+        print("A+")
+
+else:
+    print("Invalid Input")
