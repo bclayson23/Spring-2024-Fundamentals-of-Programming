72c7871528d385327413a60226b67c114e0821ed
Billy Clayson <billyclayson0@gmail.com>
2024-02-12 14:07:58 -0500

    assignment

---
+++ Week6_iterations/fizzbuzz.py
@@ -0,0 +1,9 @@
+'''
+Take an input value for n
+
+print out numbers from 1 to n with the below conditions
+1. If num is divisible by 3 --> print "FIZZ"
+2. If num is divisible by 5 --> print "BUZZ"
+3. If num is divisible by both 3 and 5 --> print "FIZZBUZZ"
+If none of the conditions are satisfied, print the number
+'''
