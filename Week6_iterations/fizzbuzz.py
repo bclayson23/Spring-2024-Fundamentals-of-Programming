bbd7e4d2933145d3b95ff8c6bf1cbc039452a536
Billy Clayson <billyclayson0@gmail.com>
2024-02-12 14:12:49 -0500

    solving

--- Week6_iterations/fizzbuzz.py
+++ Week6_iterations/fizzbuzz.py
@@ -7,3 +7,15 @@ print out numbers from 1 to n with the below conditions
 3. If num is divisible by both 3 and 5 --> print "FIZZBUZZ"
 If none of the conditions are satisfied, print the number
 '''
+
+target_num = int(input("Enter a target number: "))
+
+for num in range(1, target_num + 1):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
