7a8153abf53bdb540ec02a20dfa8efef5d918132
Billy Clayson <billyclayson0@gmail.com>
2024-02-14 14:17:27 -0500

    added notes

--- Week6_iterations/week6_hw.py
+++ Week6_iterations/week6_hw.py
@@ -4,12 +4,12 @@ target_num = int(input("Enter a number greater than one: "))
 prime_list = []
 
 if target_num <= 1:
-    print("Enter a number greater than one")
+    print("Enter a number greater than one")  # one can't be prime
 elif target_num == 2:
-    prime_list.append(target_num)
+    prime_list.append(target_num)  # 2 would be the only prime in the list
     print(prime_list)
 else:
-    for num in range(2, target_num):
+    for num in range(2, target_num):  # checking for prime number
         isPrime = True
         for i in range (2, num):
             if num % i == 0:
@@ -22,12 +22,12 @@ else:
 
 # Question 2: Write a list to print a list of all even numbers till a given target number
 target_num = int(input("Enter a number greater than one: "))
-even_list = []
+even_list = []  # setting a list
 
 if target_num <= 1:
-    print("Enter a number greater than one")
+    print("Enter a number greater than one")  # one can't have an even list
 
-for num in range(2, target_num +1, 2):
+for num in range(2, target_num +1, 2):  # skipping steps to get the evens
     even_list.append(num)
 
 print(even_list)
@@ -37,15 +37,15 @@ print(even_list)
 
 list1 = [10, 20, 30, 40, 50, 60]
 
-print(list1[1::2])
+print(list1[1::2])  # using index by every other number starting at 1 position
 
 # Question 4: Find the highest number from a given list of numbers using for loop. Do not use any in-built functions.
 
 list1 = [10, 24, 8, 187, 34, 100]
-highest_number = 0
+highest_number = 0  # setting base number
 
 for num in list1:
-    if highest_number < num:
+    if highest_number < num:  # checking if the number is higher
         highest_number = num
 
 print(highest_number)
